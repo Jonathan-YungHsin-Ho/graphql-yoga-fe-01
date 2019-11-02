37e7c7a3037d7b120e63df1136b06b2e8c72b322
Jonathan Ho <johnnyho82@gmail.com>
2019-11-02 10:24:44 -0400

    Build React app for graphql-yoga backend API

---
+++ client/src/components/Hello.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const HELLO = gql`
+  query {
+    hello
+  }
+`;
+
+export default function Hello() {
+  const { loading, error, data } = useQuery(HELLO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+
+  console.log(data);
+
+  return (
+    <div>
+      <h2>{data.hello}</h2>
+    </div>
+  );
+}
