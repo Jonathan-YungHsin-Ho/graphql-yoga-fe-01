aaf0365717e175ffc1bf7ad1f731ea34646bc92d
Jonathan Ho <johnnyho82@gmail.com>
2019-11-03 16:28:52 -0500

    Remove unused files

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 
 import Hello from './components/Hello';
-// import Input from './components/Input';
 
 function App() {
   return (
     <div className='App'>
       <h1>React App</h1>
       <Hello />
-      {/* <Input /> */}
     </div>
   );
 }
